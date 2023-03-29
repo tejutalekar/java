5fec07803e1d485e65e7aadea97c5ebd78983254
tejutalekar <128734585+tejutalekar@users.noreply.github.com>
2023-03-29 20:50:05 +0530

    Create First.java
    First Assignment

---
+++ First.java
@@ -0,0 +1,18 @@
+class First
+{ 
+	public static void main(String[] args) 
+	{
+        
+      	  int num = 55;
+        	  if(num>0)
+        	{
+            	System.out.println("The number is positive");
+        	}
+        	else
+        	{
+            	System.out.println("The number is negative");
+        	}
+        	return;
+    	}
+}
+	
